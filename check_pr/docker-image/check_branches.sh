d36cefac2b16b79f76332806b59a84a23d6419aa
Fer Madeiral <fer.madeiral@gmail.com>
2018-09-05 10:02:58 -0300

    Fix

--- check_pr/docker-image/check_branches.sh
+++ check_pr/docker-image/check_branches.sh
@@ -16,10 +16,11 @@ command -v ajv >/dev/null 2>&1 || { echo >&2 "I require ajv (https://github.com/
 
 SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
 JSON_SCHEMA="$SCRIPT_DIR/bears-schema.json"
-echo "$JSON_SCHEMA"
 if [ ! -f $JSON_SCHEMA ]; then
     echo "The json schema ($JSON_SCHEMA) cannot be found."
     exit -1
+else
+    echo "JSON schema path: $JSON_SCHEMA"
 fi
 
 MAVEN_TEST_ARGS="-Denforcer.skip=true -Dcheckstyle.skip=true -Dcobertura.skip=true -DskipITs=true -Drat.skip=true -Dlicense.skip=true -Dfindbugs.skip=true -Dgpg.skip=true -Dskip.npm=true -Dskip.gulp=true -Dskip.bower=true"
@@ -29,7 +30,7 @@ echo "Treating branch $BRANCH_NAME"
 cd pr
 
 if [ -e "bears.json" ]; then
-    if ajv test -s ../$JSON_SCHEMA -d bears.json --valid ; then
+    if ajv test -s $JSON_SCHEMA -d bears.json --valid ; then
         echo "bears.json is valid in $BRANCH_NAME"
     else
         RESULT="$BRANCH_NAME [FAILURE] (bears.json is invalid)"
